86653f75441b8269ac2f21be2408d928f588e323
emily <eap6787@gmail.com>
2022-06-14 14:55:25 -0600

    fix starting point

---
+++ client/build/precache-manifest.a7a24d1a33020f4160ac35f6ab5f6d4a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acb9c33a754d2ae6de4ab07c482b31f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bfd3338827acb6cc0bc",
+    "url": "/static/css/main.db415adc.chunk.css"
+  },
+  {
+    "revision": "2a7b005a9f5fe80ed702",
+    "url": "/static/js/2.3af34c77.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.3af34c77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bfd3338827acb6cc0bc",
+    "url": "/static/js/main.8bd8977d.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.8bd8977d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
